8ca7f9cf576518c98f8a40d1a2c4b00dd2941830
vidizb <107489313+vidizb@users.noreply.github.com>
2022-06-15 06:49:25 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from models import HelloModel
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Belajar Flask 01</p>"
+def index():
+   # membuat objek dari kelas HelloModel
+   model = HelloModel()
+   
+   # mengembalikan nilai yang diambil dari model
+   return model.getText()
 
 @app.route("/works")
 def it_works():
