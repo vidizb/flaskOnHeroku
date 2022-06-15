f7bfa190d11b07947dc73060741f0b552b6b0ff2
vidizb <107489313+vidizb@users.noreply.github.com>
2022-06-15 14:21:33 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from models import HelloModel
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-   return render_template('hello.html')
+   model = HelloModel()
+   
+   # mengirim model ke view
+   return render_template('hello.html', model=model)
 
 @app.route("/works")
 def it_works():
